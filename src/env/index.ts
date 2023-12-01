1b18d914d7e9916792c1c44dbb4e627ef5b003ea
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2023-12-01 16:07:12 -0300

    Configuração de validação encima do dotenv

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+
+import { z } from 'zod';
+
+const envSchema = z.object({
+    PORT: z.coerce.number().default(3333)
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+    console.error('Variável incorreta!');
+    throw new Error('Variável incorreta!');
+}
+
+const env = _env.data;
+
+export default env;
