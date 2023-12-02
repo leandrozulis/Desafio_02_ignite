4dcd842e3b49f88787cad16f29ac44ad252ef2cd
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2023-12-02 16:19:41 -0300

    Configurado o tipo do banco de dados para facilitar no desenvolvimento

---
+++ src/@types/Users.d.ts
@@ -0,0 +1,14 @@
+import {knex} from 'knex';
+
+declare module 'knex/types/tables' {
+       export interface Tables {
+              refeicoes: {
+                     id: string,
+                     identificacaoUser: string,
+                     nome: string,
+                     descricao: string,
+                     dieta: boolean,
+                     created_at: string
+              }
+       }
+}
